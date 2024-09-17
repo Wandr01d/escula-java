da21259dfcf9b604f0878516b2cdbda7d2b1ea32
Walter Quispe <walterarcani11@gmail.com>
2024-09-17 14:59:51 -0500

    Estructura basica del proyecto de escula-java

---
+++ src/main/java/pe/com/stefanini/group/escuelajava/EscuelaJavaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.stefanini.group.escuelajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscuelaJavaApplication.class, args);
+	}
+
+}
